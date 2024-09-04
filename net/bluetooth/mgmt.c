8ed7e01512c527753d306ba212cff5211cbb7f7f
jiaymao <quic_jiaymao@quicinc.com>
2024-09-04 20:19:11 +0800

    PENDING: Bluetooth: mgmt: Verify command pending status before removing it
    Revert commit e04d8a8d42b8 ("PENDING: Bluetooth: mgmt: Verify cmd
    pending status before removing it")
    
    Reason for revert: The code change is mistakenly added to a wrong
    function.
    
    Add a check to verify the command is still in pending list before
    removing it. In race condition, mgmt_index_removed() will first free
    the pending command if it is called accidentally before
    mgmt_add_adv_patterns_monitor_complete. Then the pending command is
    freed again in mgmt_add_adv_patterns_monitor_complete.
    
    Part of the crash call trace:
    0x0000053D: __list_del_entry_valid_or_report+0x98/0xdc
    0x0000053D: mgmt_pending_remove+0x18/0x58 [bluetooth]
    0x0000053E: mgmt_remove_adv_monitor_complete+0x80/0x108 [bluetooth]
    0x0000053E: hci_cmd_sync_work+0xbc/0x164 [bluetooth]
    
    Verify command pending status before removing it to prevent the double
    free operation.
    
    Change-Id: Iee7f66c69654ace7afc55615222ce0efb3ba2c75
    Signed-off-by: jiaymao <quic_jiaymao@quicinc.com>
    (cherry picked from commit 5979947da083c181f010dd21881289a2fd3a9895)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- net/bluetooth/mgmt.c
+++ net/bluetooth/mgmt.c
@@ -5341,9 +5341,6 @@ static void mgmt_add_adv_patterns_monitor_complete(struct hci_dev *hdev,
 
 	hci_dev_lock(hdev);
 
-	if (cmd != pending_find(MGMT_OP_REMOVE_ADV_MONITOR, hdev))
-		goto done;
-
 	rp.monitor_handle = cpu_to_le16(monitor->handle);
 
 	if (!status) {
@@ -5358,7 +5355,6 @@ static void mgmt_add_adv_patterns_monitor_complete(struct hci_dev *hdev,
 			  mgmt_status(status), &rp, sizeof(rp));
 	mgmt_pending_free(cmd);
 
-done:
 	hci_dev_unlock(hdev);
 	bt_dev_dbg(hdev, "add monitor %d complete, status %d",
 		   rp.monitor_handle, status);
@@ -5572,6 +5568,9 @@ static void mgmt_remove_adv_monitor_complete(struct hci_dev *hdev,
 
 	hci_dev_lock(hdev);
 
+	if (cmd != pending_find(MGMT_OP_REMOVE_ADV_MONITOR, hdev))
+		goto done;
+
 	cp = cmd->param;
 
 	rp.monitor_handle = cp->monitor_handle;
@@ -5585,6 +5584,7 @@ static void mgmt_remove_adv_monitor_complete(struct hci_dev *hdev,
 			  mgmt_status(status), &rp, sizeof(rp));
 	mgmt_pending_free(cmd);
 
+done:
 	hci_dev_unlock(hdev);
 	bt_dev_dbg(hdev, "remove monitor %d complete, status %d",
 		   rp.monitor_handle, status);
