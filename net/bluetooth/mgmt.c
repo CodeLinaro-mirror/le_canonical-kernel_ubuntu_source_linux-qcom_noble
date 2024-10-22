77d588357b07e1dbb9ceb5f3b8d053db87ed8ff6
Jiayang Mao <quic_jiaymao@quicinc.com>
2024-10-22 13:43:23 +0530

    BACKPORT: Bluetooth: MGMT: Fix possible crash on mgmt_index_removed If mgmt_index_removed is called while there are commands queued on cmd_sync it could lead to crashes like the bellow call trace:
    set_powered_sync+0x1c/0x70 [bluetooth]
    hci_cmd_sync_work+0xa4/0x164 [bluetooth]
    process_one_work+0x188/0x3e0
    
    So while handling mgmt_index_removed this attempts to dequeue
    commands passed as user_data to cmd_sync.
    
    Change-Id: I10a0f8db1671e8ddb8da67dacb8201cead84a916
    Signed-off-by: Jiayang Mao <quic_jiaymao@quicinc.com>
    (cherry picked from commit 0ade97ef1c0b65444de06de56d16a255ee300826)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- net/bluetooth/mgmt.c
+++ net/bluetooth/mgmt.c
@@ -5568,9 +5568,6 @@ static void mgmt_remove_adv_monitor_complete(struct hci_dev *hdev,
 
 	hci_dev_lock(hdev);
 
-	if (cmd != pending_find(MGMT_OP_REMOVE_ADV_MONITOR, hdev))
-		goto done;
-
 	cp = cmd->param;
 
 	rp.monitor_handle = cp->monitor_handle;
@@ -5584,7 +5581,6 @@ static void mgmt_remove_adv_monitor_complete(struct hci_dev *hdev,
 			  mgmt_status(status), &rp, sizeof(rp));
 	mgmt_pending_free(cmd);
 
-done:
 	hci_dev_unlock(hdev);
 	bt_dev_dbg(hdev, "remove monitor %d complete, status %d",
 		   rp.monitor_handle, status);
